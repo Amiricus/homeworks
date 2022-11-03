3ecde9423c7dcc11ab6ac1708085872076977ff1
Амир Байтулинов <amiricus@gmail.com>
2022-11-03 18:07:27 +0500

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+  return result; 
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+  return resultArr;
 }
