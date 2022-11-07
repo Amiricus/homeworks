6b7850aa73e4280590e5a7cbb64df10b8e3d5c7a
Амир Байтулинов <amiricus@gmail.com>
2022-11-07 16:16:29 +0500

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  }
 
+let studentA = new Student("Petr", "male", 20);
+let studentB = new Student("Anna", "female", 22);
+
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+      this.marks.push(mark);
+      } 
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+      this.marks.push(...marks);
+      }  
+  }
+  
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((a, b) => a + b) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
